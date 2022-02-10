8dc5950dcd5e248c0946f42f6f4a2cd07a4ea011
Bashar Hamade <bhcoding69@gmail.com>
2022-02-10 22:54:40 +0200

    make it better

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
-app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cors());
 app.use(express.json());
 
 const uri = process.env.URI;
